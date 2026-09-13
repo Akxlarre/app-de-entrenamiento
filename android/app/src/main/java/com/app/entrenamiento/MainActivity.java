75c894b830e636888ba76892ec79d0a5610bb9ef
Akxlarre <Cjentues.benjamin@gmail.com>
2026-09-13 20:12:38 -0300

    feat: capacitor integration and CI/CD pipeline

---
+++ android/app/src/main/java/com/app/entrenamiento/MainActivity.java
@@ -0,0 +1,5 @@
+package com.app.entrenamiento;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.app.entrenamiento',
+  appName: 'app-de-entrenamiento',
+  webDir: 'dist/app-de-entrenamiento/browser'
+};
+
+export default config;
