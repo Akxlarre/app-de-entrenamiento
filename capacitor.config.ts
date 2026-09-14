086cf11e007a7af15caf05aae40d56f5a7148389
Akxlarre <Cjentues.benjamin@gmail.com>
2026-09-13 21:34:45 -0300

    fix: corregir ruta webDir en capacitor.config.ts acorde al proyecto Angular

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -3,7 +3,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 const config: CapacitorConfig = {
   appId: 'com.app.entrenamiento',
   appName: 'app-de-entrenamiento',
-  webDir: 'dist/app-de-entrenamiento/browser'
+  webDir: 'dist/koa-tmp-app-de-entrenamiento-934c46560e95e62c/browser'
 };
 
 export default config;
